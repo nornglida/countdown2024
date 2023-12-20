a1cfae440f78224c16150b774ed840ec6d778904
Lida NORNG <nornglida@ymail.com>
2023-12-20 10:15:59 +0700

    commit 5 files first time

---
+++ script.js
@@ -0,0 +1,33 @@
+const year = new Date().getFullYear();
+const newYear = new Date(year + 1, 0, 1).getTime();
+
+
+console.log(newYear);
+
+// countdown
+let timer = setInterval(function () {
+
+  // get today's date
+  const today = new Date().getTime();
+  // get the difference
+  let diff = newYear - today;
+
+  // math
+  let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  // display
+  document.getElementById("timer").innerHTML =
+    "<div class=\"days\"> \
+  <div class=\"numbers\">" + days + "</div>days</div> \
+<div class=\"hours\"> \
+  <div class=\"numbers\">" + hours + "</div>hours</div> \
+<div class=\"minutes\"> \
+  <div class=\"numbers\">" + minutes + "</div>minutes</div> \
+<div class=\"seconds\"> \
+  <div class=\"numbers\">" + seconds + "</div>seconds</div> \
+</div>";
+
+}, 1000);
